f154223b7e6650fdfd6cd2e873b4c86f8493d399
pikktorr <viktor.rozgonyi@gmail.com>
2020-04-20 15:42:12 +0200

    foundation

--- 03_foundation.js
+++ 03_foundation.js
@@ -98,3 +98,76 @@ const obj2 = {
 importantPerson(); //undefined
 obj1.importantPerson(); //'Cassy'
 obj2.importantPerson(); //'Jacob'
+
+//LEXICAL VS DINAMYC SCOPE
+//In Javascript our Lexical Scope (available data + variables where the function was defined)
+//determines our available variables. Not where the function is called (Dynamic Scope)
+//'this' - doesn't matter where it's written, it matters how the function was called
+const a = function () {
+  console.log("a", this);
+  const b = function () {
+    console.log("b", this);
+    const c = {
+      hi: function () {
+        console.log("c", this);
+      },
+    };
+    c.hi(); //'c' Object [c] {hi: [Function: hi]}
+  };
+  b(); //'b' Object [global]
+};
+a(); //'a' Object [global]
+
+//CONFUSING 'THIS' REFERENCING
+const obj3 = {
+  name: "Viktor",
+  speak() {
+    console.log("obj3 - a", this);
+    var anotherFunc = function () {
+      console.log("obj3 - b", this);
+    };
+    anotherFunc(); // 'b' Object [global] - because we invoke it from GLOBAL through obj3.speak();
+    // only speak() is bind to obj3, anotherFunc() is not directly, because it's stored in a variable
+  },
+};
+obj3.speak(); // 'a' Object [obj3]
+
+const obj4 = {
+  name: "Viktor",
+  speak() {
+    console.log("obj4 - a", this);
+    var anotherFunc = function () {
+      console.log("obj4 - b", this);
+    };
+    return anotherFunc.bind(this)();
+    // 'b' Object [obj4] - when invoking the function, bind 'this' to surrounding Object
+  },
+};
+obj4.speak(); // 'a' Object [obj4]
+
+const obj5 = {
+  name: "Viktor",
+  speak() {
+    console.log("obj5 - a", this);
+    var itself = this; //referring to surrounding Object
+    var anotherFunc = function () {
+      console.log("obj5 - b", itself); //pointing to referred 'this'
+    };
+    anotherFunc(); // 'a' Object [obj5]
+  },
+};
+obj5.speak(); // 'a' Object [obj5]
+
+//ES6 way preventing inner 'this' to point to global
+const obj6 = {
+  name: "Viktor",
+  speak() {
+    console.log("obj6 - a", this);
+    var anotherFunc = () => {
+      //arrow function Lexically binds 'this' to the surrounding Object
+      console.log("obj6 - b", this);
+    };
+    anotherFunc(); // 'b' Object [obj6]
+  },
+};
+obj6.speak(); // 'a' Object [obj6]
---
+++ 04_foundation.js
@@ -0,0 +1,115 @@
+//CALL(), APPLY(), BIND()
+const wizard = {
+  name: "Dumbledore",
+  health: 50,
+  fullHeal() {
+    return (this.health = 100);
+  },
+  heal(num1, num2) {
+    return (this.health += num1 + num2);
+  },
+};
+const archer = {
+  name: "Archer Henry",
+  health: 30,
+};
+
+console.log("wizard health: ", wizard.health); // wizard health: 50
+wizard.fullHeal(); //wizard uses healing to max health
+console.log("wizard health: ", wizard.health); // wizard health: 100
+
+console.log("archer health: ", archer.health); // archer health: 30
+wizard.heal.call(archer, 30, 20); //archer uses the wizards healing
+//call() can use arguments too, after the used object
+wizard.heal.apply(archer, [5, 10]); //archer uses the wizards healing
+//apply() uses arguments as an array of parameters
+const healArcher = wizard.heal.bind(archer, 3, 1); //archer uses the wizards healing
+//bind() doesn't run the function, it only allows to store, then run it
+healArcher();
+console.log("archer health: ", archer.health); // archer health: 99
+
+//GET THE MAX NUMBER FROM ARRAY
+//Manual solution
+const array = [3, 2, 1, 72, 0, 43, 5, 6];
+function getMaxNumber(arr) {
+  let arr2 = [...arr];
+  for (let i = 0; i < arr2.length - 1; i++) {
+    let [a, b] = [arr2[i], arr2[i + 1]];
+    if (a > b) {
+      [arr2[i], arr2[i + 1]] = [b, a];
+    }
+  }
+  let maxNumber = arr2[arr2.length - 1];
+  return maxNumber;
+}
+getMaxNumber(array); // 72
+
+//Solution with apply() - avoid if array has too many elements
+function getMaxOfArray(arr) {
+  return Math.max.apply(null, arr);
+}
+
+//Solution with reduce()
+const reduceToMax = array.reduce((accumulator, currentValue) => {
+  //on every iteration the reducer compares the current value to the largest ever number
+  // acc  current
+  // 3    2
+  // 3    1
+  // 3    72
+  // 72   0
+  // 72   43
+  // 72   5
+  // 72   6 - this will be last Math.max(72, 6)
+  return Math.max(accumulator, currentValue);
+  //Math.max() returns the last comparison, with the largest number
+});
+console.log(reduceToMax); // 72
+
+//FUNCTION CURRYING WITH BIND()
+//partially giving function parameter
+function multiply(a, b) {
+  return a * b;
+}
+let multiplyByTwo = multiply.bind(this, 2); //binds 'this' to multiply() with one parameter
+multiplyByTwo(3); // 6 - calling with one parameter(3) and the already bound parameter(2)
+
+//EXERCISE - 'THIS'
+var a = {
+  name: "Tom Nook",
+  say() {
+    console.log(this);
+  },
+};
+var b = {
+  name: "Tommy",
+  say() {
+    return function () {
+      console.log(this);
+    };
+  },
+};
+var c = {
+  name: "Timmy",
+  say() {
+    return () => console.log(this);
+  },
+};
+a.say(); // Object [a]
+b.say(); // Function {console.log(this)}
+b.say()(); // Object [global] - Dynamically scoped
+c.say(); // ()=>{console.log(this)}
+c.say()(); // Object [c] - arrow function binds 'this' to object - Lexically scoped
+
+//EXERCISE II
+const character = {
+  name: "Isabelle",
+  getCharacter() {
+    return this.name;
+  },
+};
+const giveMeName = character.getCharacter.bind(character);
+console.log(giveMeName());
+
+//CONTEXT VS SCOPE
+//Context is how a function is invoked with the value of 'this' keyword
+//Scope refers to the visibility of variables 
