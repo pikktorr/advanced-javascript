ae5c7635d9273a56865aee4953de6c48953caabe
pikktorr <viktor.rozgonyi@gmail.com>
2020-05-05 12:23:11 +0200

    async await

--- 13_async.js
+++ 13_async.js
@@ -35,13 +35,73 @@ const promise4 = new Promise((resolve, reject) => {
 Promise.all([promise, promise2, promise3, promise4]).then((values) => values);
 
 // CALLING API
-const urls = ["https://jsonplaceholder.typicode.com/users"];
-Promise.all(
-  urls.map((url) => {
-    return fetch(url).then((response) => response.json());
-  })
-)
+// 1. fetch data
+fetch("https://api.covid19api.com/live/country/hungary") // returns only a Promise
+  // 2. parse data to json
+  .then((response) => response.json()) // got the data in json
+  // 3. do something with data e.g. console.log
   .then((result) => {
-    console.log(result[0]);
-  })
+    // log the data
+    result[result.length - 1]; // last status
+  });
+
+// MULTIPLE CALL
+const urls0 = [
+  "https://jsonplaceholder.typicode.com/users",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/albums",
+];
+
+const fetchUrls = urls0.map((url) => {
+  return fetch(url).then((response) => response.json());
+});
+
+Promise.all(fetchUrls)
+  .then((result) => result)
   .catch(() => console.log("error"));
+
+// ES6 ASYNC AWAIT
+
+// classic promise chaining
+// movePlayer(100, "left")
+//   .then(() => movePlayer(200, "right"))
+//   .then(() => movePlayer(100, "left"))
+//   .then(() => movePlayer(300, "left"));
+
+// need to declare a function as async
+// looks like synchronous, can store steps
+async function playerStart() {
+  await movePlayer(200, "right"); // pause function until it has something, awaiting response
+  await movePlayer(200, "right");
+  await movePlayer(100, "left");
+  await movePlayer(300, "left");
+}
+
+async function fetchUsers() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users"); // paused until we get response
+  const data = await response.json();
+}
+fetchUsers();
+
+const urls = [
+  "https://jsonplaceholder.typicode.com/users",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/albums",
+];
+
+const getData = async function () {
+  // try-catch block
+  // tries first block
+  try {
+    // destructuring array - [urls[0], urls[1], urls[2]] = [users, posts, albums]
+    const [users, posts, albums] = await Promise.all(
+      urls.map((url) => fetch(url).then((response) => response.json()))
+    );
+    console.log("users", users);
+    console.log("posts", posts);
+    console.log("albums", albums);
+  } catch (error) {
+    //if anything fails, returns catch block
+    console.log("error", error);
+  }
+};
