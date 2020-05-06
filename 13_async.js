595594f77af2b9fc60e5b7781b40347afa848acb
pikktorr <viktor.rozgonyi@gmail.com>
2020-05-06 15:17:23 +0200

    fetch, promises, for await of

--- 13_async.js
+++ 13_async.js
@@ -1,10 +1,10 @@
 // ASYNCHRONOUS JAVASCRIPT
 // functions we execute later
 
+const fetch = require("node-fetch");
+
 // PROMISES
 // fulfilled, rejected, pending
-
-const fetch = require("node-fetch");
 const promise = new Promise((resolve, reject) => {
   if (true) {
     resolve("Stuff worked!");
@@ -12,96 +12,102 @@ const promise = new Promise((resolve, reject) => {
     reject("Error, it broke.");
   }
 });
-
 promise
   .then((result) => result + "!")
   .then((result2) => result2 + "?")
   .then((result3) => result3 + "!")
   .catch("error");
-// catch should be the last to handle all the error before it
+// catch should be the last to handle all the errors before it
 
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(resolve, 100, "hellloo");
 });
-
 const promise3 = new Promise((resolve, reject) => {
   setTimeout(resolve, 1000, "hiii");
 });
-
 const promise4 = new Promise((resolve, reject) => {
   setTimeout(resolve, 3000, "Last man standing");
 });
-
 Promise.all([promise, promise2, promise3, promise4]).then((values) => values);
 
-// CALLING API
-// 1. fetch data
+// CALLING A SINGLE API
+// 1. fetch url
 fetch("https://api.covid19api.com/live/country/hungary") // returns only a Promise
   // 2. parse data to json
   .then((response) => response.json()) // got the data in json
   // 3. do something with data e.g. console.log
   .then((result) => {
     // log the data
-    result[result.length - 1]; // last status
-  });
+    console.log(result[result.length - 1]); // last status
+  })
+  .catch((error) => console.log("error", error));
 
-// MULTIPLE CALL
-const urls0 = [
-  "https://jsonplaceholder.typicode.com/users",
-  "https://jsonplaceholder.typicode.com/posts",
-  "https://jsonplaceholder.typicode.com/albums",
+// MULTIPLE CALLS
+// when using multiple urls, have to use Promise.all to handle all promises with .then()
+const urls = [
+  "https://jsonplaceholder.typicode.com/users/1",
+  "https://jsonplaceholder.typicode.com/posts/2",
+  "https://jsonplaceholder.typicode.com/albums/3",
 ];
-
-const fetchUrls = urls0.map((url) => {
-  return fetch(url).then((response) => response.json());
-});
-
-Promise.all(fetchUrls)
-  .then((result) => result)
-  .catch(() => console.log("error"));
+Promise.all(
+  // .map().then is not working on it's own
+  urls.map((url) => fetch(url).then((response) => response.json()))
+)
+  // Promise.all().then() is working
+  .then((result) => console.log(result))
+  .catch((error) => console.log("error", error))
+  // ES9 FINALLY()
+  .finally(() =>
+    console.log(
+      "Fetching is over. Logging this whether it was successful or not."
+    )
+  );
 
 // ES6 ASYNC AWAIT
-
-// classic promise chaining
-// movePlayer(100, "left")
-//   .then(() => movePlayer(200, "right"))
-//   .then(() => movePlayer(100, "left"))
-//   .then(() => movePlayer(300, "left"));
-
 // need to declare a function as async
 // looks like synchronous, can store steps
-async function playerStart() {
-  await movePlayer(200, "right"); // pause function until it has something, awaiting response
-  await movePlayer(200, "right");
-  await movePlayer(100, "left");
-  await movePlayer(300, "left");
-}
-
-async function fetchUsers() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users"); // paused until we get response
-  const data = await response.json();
-}
-fetchUsers();
-
-const urls = [
-  "https://jsonplaceholder.typicode.com/users",
-  "https://jsonplaceholder.typicode.com/posts",
-  "https://jsonplaceholder.typicode.com/albums",
-];
-
-const getData = async function () {
-  // try-catch block
-  // tries first block
+// SINGLE CALL
+const fetchSingleUrl = async function (url) {
   try {
-    // destructuring array - [urls[0], urls[1], urls[2]] = [users, posts, albums]
-    const [users, posts, albums] = await Promise.all(
-      urls.map((url) => fetch(url).then((response) => response.json()))
-    );
-    console.log("users", users);
-    console.log("posts", posts);
-    console.log("albums", albums);
-  } catch (error) {
-    //if anything fails, returns catch block
-    console.log("error", error);
+    const response = await fetch(url);
+    const result = await response.json();
+    console.log("single:", result);
+  } catch {
+    console.log("error");
   }
 };
+fetchSingleUrl(urls[0]);
+
+// MULTIPLE CALLS
+const fetchMultipleUrls = async function (url) {
+  try {
+    const results = await Promise.all(
+      url.map((link) => fetch(link).then((response) => response.json()))
+    );
+    console.log("multiple:", results);
+  } catch {
+    console.log("error");
+  }
+};
+fetchMultipleUrls(urls);
+
+// ES9 FOR AWAIT LOOPING
+// in this case Promise.all() is unnecessary
+const fetchWithLoop = async function (url) {
+  try {
+    // returning an array of promises in json - not using Promise.all()
+    const arrayOfPromises = url.map((link) =>
+      fetch(link).then((response) => response.json())
+    );
+    // in this case using the await before the returned promises
+    for await (let data of arrayOfPromises) {
+      // looping through data with await like it is synchronous
+      console.log("for await of:", data);
+    }
+  } catch {
+    console.log("error");
+  }
+};
+fetchWithLoop(urls);
+
+// JOB QUEUE
